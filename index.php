3f36943023e2b6d8e13a8dc8f86eeb8fab4afefc
Manuth Ariyadasa <manuthayuka03@gmail.com>
2026-07-25 17:56:11 +0530

    Initiating the blog application

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Blog App</title>
+</head>
+<body>
+    <h1>My Blog Application</h1>
+    <p>My first PHP project is running.</p>
+
+    <?php
+        echo "<p>This text was created by PHP.</p>";
+    ?>
+</body>
+</html>
